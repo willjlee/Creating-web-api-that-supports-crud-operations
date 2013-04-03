d0920d1c6d9453b722067c2130d68332d94290cc
William <willjlee@fake.email>
2013-04-03 16:41:26 -0400

    first commit

---
+++ CRUDAPI/Controllers/ProductsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CRUDAPI.Models;
+
+namespace CRUDAPI.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        static readonly IProductRepository repository = new ProductRepository();
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return repository.GetAll();
+        }
+
+        public Product GetProduct(int id)
+        {
+            Product item = repository.Get(id);
+            if (item == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return item;
+        }
+
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return repository.GetAll().Where(
+                p => string.Equals(p.Category, category, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public HttpResponseMessage PostProduct(Product item) //returns an HTTP response to client
+            //adds new product
+        {
+            item = repository.Add(item);
+            var response = Request.CreateResponse<Product>(HttpStatusCode.Created, item);//indicate item creation
+
+            string uri = Url.Link("DefaultApi", new { id = item.Id });
+            response.Headers.Location = new Uri(uri);//indicate item location
+            return response;
+        }
+
+        public void PutProduct(int id, Product product)
+            //updates product
+        {
+            product.Id = id;
+            if (!repository.Update(product))
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+
+        public void DeleteProduct(int id)
+            //deletes a product
+        {
+            Product item = repository.Get(id);//check that item exists
+            if (item == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            repository.Remove(id);
+        }
+    }
+}
---
+++ CRUDAPI/Models/ProductRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUDAPI.Models
+{
+    public class ProductRepository : IProductRepository //uses interface
+    {
+        //initialize things
+        private List<Product> products = new List<Product>();
+        private int _nextId = 1;
+
+        public ProductRepository()
+            //note that this uses the Add() method defined below
+        {
+            Add(new Product { Name = "Tomato soup", Category = "Groceries", Price = 1.39M });
+            Add(new Product { Name = "Yo-yo", Category = "Toys", Price = 3.75M });
+            Add(new Product { Name = "Hammer", Category = "Hardware", Price = 16.99M });
+        }
+
+        public IEnumerable<Product> GetAll()
+            //returns all products
+        {
+            return products;
+        }
+
+        public Product Get(int id)
+            //returns product by id
+        {
+            return products.Find(p => p.Id == id);
+        }
+
+        public Product Add(Product item)
+            //adds a new item
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+            item.Id = _nextId++;
+            products.Add(item);
+            return item;
+        }
+
+        public void Remove(int id)
+            //removes item by id
+        {
+            products.RemoveAll(p => p.Id == id);
+        }
+
+        public bool Update(Product item)
+            //finds item by id and replaces it with new info
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+            int index = products.FindIndex(p => p.Id == item.Id);
+            if (index == -1)
+            {
+                return false;
+            }
+            products.RemoveAt(index);
+            products.Add(item);
+            return true;
+        }
+
+    }
+}
